a2763eaeea3bbc2c980e63a47c67eb7c026d70f3
Alpinist051 <alpinistnex@outlook.com>
2026-04-01 05:10:14 -0400

    initial commit

---
+++ src/worker.ts
@@ -0,0 +1,26 @@
+import { ProcessVideoTrackOptions } from 'src';
+import { runSegmenter, options, SegmenterStats } from './segmenter';
+
+self.onmessage = ({ data }) => {
+    console.log(`[virtual-background] worker onmessage`, data);
+    const { name } = data as { name: string };
+    if (name === 'options') {
+        const { options: opts } = data as { options: ProcessVideoTrackOptions };
+        Object.assign(options, opts);
+    } else if (name === 'runSegmenter') {
+        const {
+            canvas,
+            readable,
+            options: opts,
+        } = data as {
+            canvas: OffscreenCanvas;
+            readable: ReadableStream;
+            options: ProcessVideoTrackOptions;
+        };
+        runSegmenter(canvas, readable, opts, (stats: SegmenterStats) => {
+            self.postMessage({ name: 'stats', stats });
+        }).catch((err: unknown) => {
+            console.error(`[virtual-background] video error: ${(err as Error).message}`);
+        });
+    }
+};
